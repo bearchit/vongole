0ef4592cdf4c2814e126a25258012e76b4502672
Caden <caden@kakao.com>
2012-09-29 02:20:56 +0900

    메뉴 <-> 음식점 다대다 관계 추가

--- app/models/menu.rb
+++ app/models/menu.rb
@@ -1,10 +1,17 @@
 class Menu
   include Mongoid::Document
 
+  # Association
+  has_and_belongs_to_many :restaurants, index: true
+
+  # Field
   field :name, type: String
   field :price, type: Integer
   field :like, type: Integer
   field :dislike, type: Integer
+
+
+  # ============================================================
 
   class << self
     def pick
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,11 +1,22 @@
 class Restaurant
   include Mongoid::Document
 
+  # Association
+  has_and_belongs_to_many :menus, index: true
+
+  # Field
   field :name, type: String
   field :address, type: String
   field :tel, type: String
   field :like, type: Integer
   field :dislike, type: Integer
+  field :location, type: Array
+
+  # Index
+  index({ location: "2d" })
+
+
+  # ============================================================
 
   class << self
     def pick
