9bbffef806c0b95a9bf4097e38becabafb0afda7
Caden <caden@kakao.com>
2012-09-28 23:11:09 +0900

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Vongole::Application.config.secret_token = '445814aca7c53f58e87d77801157738956102e784dc45fdbf5f472d617016448aacf7b539d101000061d9c1a91c91d25ab965611993eb821a59aa5c98b1fcc0b'
